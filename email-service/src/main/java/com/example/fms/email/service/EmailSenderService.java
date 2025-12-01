f3d48eee415a9ed67467ee83465afe7d5666809b
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2025-12-01 22:47:44 +0530

    Fixed Email service

--- email-service/src/main/java/com/example/fms/email/service/EmailSenderService.java
+++ email-service/src/main/java/com/example/fms/email/service/EmailSenderService.java
@@ -3,6 +3,7 @@ package com.example.fms.email.service;
 import com.example.fms.common.dto.EmailEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,12 @@ public class EmailSenderService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String mailUser;
+
     public void sendEmail(EmailEvent event) {
+
+        log.debug("EMAIL SERVICE → Using SMTP user: {}", mailUser);
 
         try {
             SimpleMailMessage message = new SimpleMailMessage();
@@ -22,12 +28,17 @@ public class EmailSenderService {
             message.setSubject(event.subject());
             message.setText(event.body());
 
+            log.debug("Attempting to send email to: {}", event.to());
+
             mailSender.send(message);
 
             log.info("Email sent successfully to {}", event.to());
 
         } catch (Exception ex) {
-            log.error("Failed to send email to {} :: {}", event.to(), ex.getMessage());
+            log.error("EMAIL SENDING FAILED");
+            log.error("To: {}", event.to());
+            log.error("SMTP User: {}", mailUser);
+            log.error("Reason: {}", ex.getMessage());
         }
     }
 }
